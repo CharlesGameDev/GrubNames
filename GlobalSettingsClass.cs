2c7b57d71c317bf4136583432f70a52a317ef205
lim <lgrant181@gmail.com>
2023-06-20 15:31:40 +1000

    Grub names are now read from the GrubNames.GlobalSettings.json file

---
+++ GlobalSettingsClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrubNames
+{
+    public class GlobalSettingsClass
+    {
+        public string Grub_Crossroads_Acid = "0";
+        public string Grub_Crossroads_Center = "1";
+        public string Grub_Crossroads_Stag = "2";
+        public string Grub_Crossroads_Spike = "3";
+        public string Grub_Crossroads_Guarded = "4";
+        public string Grub_Greenpath_Cornifer = "5";
+        public string Grub_Greenpath_Journal = "6";
+        public string Grub_Greenpath_MMC = "7";
+        public string Grub_Greenpath_Stag = "8";
+        public string Grub_Fog_Canyon = "9";
+        public string Grub_Fungal_Bouncy = "10";
+        public string Grub_Fungal_Spore_Shroom = "11";
+        public string Grub_Deepnest_Mimic = "12";
+        public string Grub_Deepnest_Nosk = "13";
+        public string Grub_Deepnest_Spike = "14";
+        public string Grub_Dark_Deepnest = "15";
+        public string Grub_Beasts_Den = "16";
+        public string Grub_Kingdoms_Edge_Oro = "17";
+        public string Grub_Kingdoms_Edge_Camp = "18";
+        public string Grub_Hive_External = "19";
+        public string Grub_Hive_Internal = "20";
+        public string Grub_Basin_Requires_Wings = "21";
+        public string Grub_Basin_Requires_Dive = "22";
+        public string Grub_Waterways_Main = "23";
+        public string Grub_Ismas_Grove = "24";
+        public string Grub_Waterways_Requires_Tram = "25";
+        public string Grub_City_of_Tears_Left = "26";
+        public string Grub_Soul_Sanctum = "27";
+        public string Grub_Watchers_Spire = "28";
+        public string Grub_City_of_Tears_Guarded = "29";
+        public string Grub_Kings_Station = "30";
+        public string Grub_Resting_Grounds = "31";
+        public string Grub_Crystal_Peak_Below_Chest = "32";
+        public string Grub_Crystallized_Mound = "33";
+        public string Grub_Crystal_Peak_Spike = "34";
+        public string Grub_Crystal_Peak_Mimic = "35";
+        public string Grub_Crystal_Peak_Crushers = "36";
+        public string Grub_Crystal_Heart = "37";
+        public string Grub_Hallownest_Crown = "38";
+        public string Grub_Howling_Cliffs = "39";
+        public string Grub_Queens_Gardens_Stag = "40";
+        public string Grub_Queens_Gardens_Marmu = "41";
+        public string Grub_Queens_Gardens_Top = "42";
+        public string Grub_Collector_1 = "43";
+        public string Grub_Collector_2 = "44";
+        public string Grub_Collector_3 = "45";
+
+    }
+}
